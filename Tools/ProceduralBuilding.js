37cdc1a74c8ef61e43944ad8cbf14b3c9405bbf6
Edgar Zambrano Rodríguez <114836515+EDGARZR54@users.noreply.github.com>
2025-08-18 17:31:35 -0600

    Update ProceduralBuilding.js

--- Tools/ProceduralBuilding.js
+++ Tools/ProceduralBuilding.js
@@ -1,4 +1,4 @@
-class MainProject {
+class ProceduralCityGenerator {
     constructor() {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
